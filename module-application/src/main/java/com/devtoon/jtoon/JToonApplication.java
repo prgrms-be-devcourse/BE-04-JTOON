015782a89edb3ff8f378372e4892d839e71965d2
HyuckJuneHong <jjj8337721@naver.com>
2023-08-27 01:14:00 +0900

    init: module setting

---
+++ module-application/src/main/java/com/devtoon/jtoon/JToonApplication.java
@@ -0,0 +1,11 @@
+package com.devtoon.jtoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JToonApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JToonApplication.class, args);
+	}
+}
