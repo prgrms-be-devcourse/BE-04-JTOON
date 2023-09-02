8d31056f55d9274ecb85cfe807c28eed2f3a0ebc
HyuckJuneHong <jjj8337721@naver.com>
2023-09-02 20:13:24 +0900

    [JT-34] chore: 결제 컨트롤러 mvc에서 restapi로 변경
    - 클래스명 변경
    - @Controller -> @RestController 어노테이션 변경
    - URL 등록

--- module-application/src/main/java/com/devtoon/jtoon/payment/presentation/PaymentController.java
+++ module-application/src/main/java/com/devtoon/jtoon/payment/presentation/PaymentController.java
@@ -1,12 +1,14 @@
 package com.devtoon.jtoon.payment.presentation;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.siot.IamportRestClient.IamportClient;
 
-@Controller
-public class PaymentViewController {
+@RestController
+@RequestMapping("/api/v1/payments")
+public class PaymentController {
 
 	@Value("${pg.kg-inicis.rest-api-key}")
 	private String REST_API_KEY;
@@ -16,7 +18,7 @@ public class PaymentViewController {
 
 	private final IamportClient iamportClient;
 
-	public PaymentViewController() {
+	public PaymentController() {
 		this.iamportClient = new IamportClient(REST_API_KEY, REST_API_SECRET);
 	}
 }
