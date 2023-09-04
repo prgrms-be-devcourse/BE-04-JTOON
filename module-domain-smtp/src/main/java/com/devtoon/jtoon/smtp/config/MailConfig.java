7f88c6cd7225b7f3bd2c2b64cbeca21e87457089
ymkim97 <rladudaud97@naver.com>
2023-09-04 15:22:23 +0900

    [JT-28] refactor: SMTP 인프라 분리

--- module-domain-smtp/src/main/java/com/devtoon/jtoon/smtp/config/MailConfig.java
+++ module-domain-smtp/src/main/java/com/devtoon/jtoon/smtp/config/MailConfig.java
@@ -1,4 +1,4 @@
-package com.devtoon.jtoon.global.config;
+package com.devtoon.jtoon.smtp.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
