538f10f2cd383446fad9b82bf94971ca19e4d122
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-09-04 22:12:50 +0900

    [JT-53] feat: 웹툰 도메인 구현

--- module-domain/src/main/java/com/devtoon/jtoon/webtoon/repository/WebtoonRepository.java
+++ module-domain/src/main/java/com/devtoon/jtoon/webtoon/repository/WebtoonRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devtoon.jtoon.webtoon.entity.Webtoon;
 
 public interface WebtoonRepository extends JpaRepository<Webtoon, Long> {
+
+	boolean existsByTitle(String title);
 }
--- module-domain/src/main/java/com/devtoon/jtoon/webtoon/request/CreateWebtoonReq.java
+++ module-domain/src/main/java/com/devtoon/jtoon/webtoon/request/CreateWebtoonReq.java
@@ -1,4 +1,34 @@
 package com.devtoon.jtoon.webtoon.request;
 
-public record CreateWebtoonReq() {
+import java.util.Set;
+
+import com.devtoon.jtoon.member.entity.Member;
+import com.devtoon.jtoon.webtoon.entity.AgeLimit;
+import com.devtoon.jtoon.webtoon.entity.DayOfWeek;
+import com.devtoon.jtoon.webtoon.entity.Genre;
+import com.devtoon.jtoon.webtoon.entity.Webtoon;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
+public record CreateWebtoonReq(
+	@NotBlank String title,
+	@NotBlank String description,
+	@NotBlank Set<DayOfWeek> dayOfWeeks,
+	@NotBlank Set<Genre> genres,
+	@NotBlank AgeLimit ageLimit,
+	@Min(0) int cookieCount
+	) {
+
+	public Webtoon toEntity(Member member) {
+		return Webtoon.builder()
+			.title(this.title)
+			.description(this.description)
+			.dayOfWeeks(this.dayOfWeeks)
+			.genres(this.genres)
+			.ageLimit(this.ageLimit)
+			.cookieCount(this.cookieCount)
+			.author(member)
+			.build();
+	}
 }
