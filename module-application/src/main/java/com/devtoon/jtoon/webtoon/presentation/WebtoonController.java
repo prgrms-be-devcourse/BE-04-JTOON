4d94823f176c0272f81b89a5c95001f11aa546b1
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-09-04 22:14:41 +0900

    [JT-53] feat: 웹툰 등록 컨트롤러 구현

--- module-application/src/main/java/com/devtoon/jtoon/webtoon/presentation/WebtoonController.java
+++ module-application/src/main/java/com/devtoon/jtoon/webtoon/presentation/WebtoonController.java
@@ -1,9 +1,29 @@
 package com.devtoon.jtoon.webtoon.presentation;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devtoon.jtoon.webtoon.application.WebtoonService;
+import com.devtoon.jtoon.webtoon.request.CreateWebtoonReq;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/webtoons")
 public class WebtoonController {
+
+	private final WebtoonService webtoonService;
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createWebtoon(@RequestBody @Valid CreateWebtoonReq request) {
+		Long memberId = 1L;
+		webtoonService.createWebtoon(memberId, request);
+	}
 }
