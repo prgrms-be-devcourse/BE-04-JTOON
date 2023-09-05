05a5e7d3cc05c4904b3b6b02b20fd3966d6d3860
ymkim97 <rladudaud97@naver.com>
2023-09-05 19:06:43 +0900

    [JT-29] feat: Security Config 작성

--- module-application/src/main/java/com/devtoon/jtoon/security/config/WebSecurityConfiguration.java
+++ module-application/src/main/java/com/devtoon/jtoon/security/config/WebSecurityConfiguration.java
@@ -4,13 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.devtoon.jtoon.security.filter.JwtAuthenticationFilter;
+import com.devtoon.jtoon.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfiguration {
+
+	private final JwtProvider jwtProvider;
 
 	@Bean
 	public PasswordEncoder encoder() {
@@ -22,7 +32,11 @@ public class WebSecurityConfiguration {
 		http
 			.authorizeHttpRequests(request -> request
 				.anyRequest().permitAll())
-			;
+			.csrf(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
+		;
 		return http.build();
 	}
 }
