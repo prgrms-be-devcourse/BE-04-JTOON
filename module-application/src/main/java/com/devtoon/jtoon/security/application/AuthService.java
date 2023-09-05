b7bf95d8706a8ebee67ea3a91cc77892c7283336
ymkim97 <rladudaud97@naver.com>
2023-09-05 19:09:10 +0900

    [JT-29] feat: JWT 로그인 기능 구현

---
+++ module-application/src/main/java/com/devtoon/jtoon/security/application/AuthService.java
@@ -0,0 +1,36 @@
+package com.devtoon.jtoon.security.application;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.devtoon.jtoon.member.entity.Member;
+import com.devtoon.jtoon.member.repository.MemberRepository;
+import com.devtoon.jtoon.security.jwt.JwtProvider;
+import com.devtoon.jtoon.security.request.LogInReq;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
+	private final JwtProvider jwtProvider;
+
+	public String login(LogInReq logInReq) {
+		Member member = memberRepository.findByEmail(logInReq.email()).orElseThrow(
+			() -> new BadCredentialsException("너 안돼!")
+		);
+
+		if (!isPasswordSame(logInReq.password(), member.getPassword())) {
+			throw new BadCredentialsException("너 안돼!");
+		}
+
+		return jwtProvider.generateToken(logInReq.email());
+	}
+
+	public boolean isPasswordSame(String rawPassword, String memberPassword) {
+		return passwordEncoder.matches(rawPassword, memberPassword);
+	}
+}
