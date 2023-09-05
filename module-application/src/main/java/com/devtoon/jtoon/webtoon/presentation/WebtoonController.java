c31a0fee39a00e5429a0a7c0665962929ccfda8d
kmebin <blowhuiee@gmail.com>
2023-09-05 14:44:04 +0900

    [JT-38] feat: 상태 코드 201로 수정

--- module-application/src/main/java/com/devtoon/jtoon/webtoon/presentation/WebtoonController.java
+++ module-application/src/main/java/com/devtoon/jtoon/webtoon/presentation/WebtoonController.java
@@ -1,8 +1,10 @@
 package com.devtoon.jtoon.webtoon.presentation;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devtoon.jtoon.webtoon.application.WebtoonService;
@@ -19,6 +21,7 @@ public class WebtoonController {
 	private final WebtoonService webtoonService;
 
 	@PostMapping("/{webtoonId}")
+	@ResponseStatus(HttpStatus.CREATED)
 	public void createEpisode(@PathVariable Long webtoonId, @Valid CreateEpisodeReq req) {
 		webtoonService.createEpisode(webtoonId, req);
 	}
