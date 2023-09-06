8018a49747b0812e7457174bf53c929785f082d8
HyuckJuneHong <jjj8337721@naver.com>
2023-09-06 15:59:08 +0900

    [JT-52] fix: Member Repository import 수정

--- module-domain/src/main/java/com/devtoon/jtoon/member/repository/MemberRepository.java
+++ module-domain/src/main/java/com/devtoon/jtoon/member/repository/MemberRepository.java
@@ -9,5 +9,7 @@ import com.devtoon.jtoon.member.entity.Member;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 	boolean existsByEmail(String email);
 
+	Optional<Member> findByPhone(String phone);
+
 	Optional<Member> findByEmail(String email);
 }
