dd7307e2ee6e68980a6a8f83acfc679cbb7141e8
HyuckJuneHong <jjj8337721@naver.com>
2023-09-12 00:15:05 +0900

    [JT-60] style : api/v1 버저닝 제거

--- module-application/src/main/java/com/devtoon/jtoon/payment/presentation/PaymentController.java
+++ module-application/src/main/java/com/devtoon/jtoon/payment/presentation/PaymentController.java
@@ -20,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/payments")
+@RequestMapping("/payments")
 public class PaymentController {
 
 	private final PaymentService paymentService;
