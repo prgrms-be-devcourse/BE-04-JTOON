ccd79f8d6a6c26996b962db5232c77f0ac0cd4d1
ymkim97 <rladudaud97@naver.com>
2023-09-12 17:18:59 +0900

    [JT-65] hotfix: webIgnore 삭제

--- module-application/src/main/java/com/devtoon/jtoon/security/config/SecurityConfig.java
+++ module-application/src/main/java/com/devtoon/jtoon/security/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,7 +17,6 @@ import com.devtoon.jtoon.security.application.JwtService;
 import com.devtoon.jtoon.security.filter.JwtAuthenticationFilter;
 import com.devtoon.jtoon.security.handler.OAuth2FailureHandler;
 import com.devtoon.jtoon.security.handler.OAuth2SuccessHandler;
-
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -35,13 +33,6 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return web -> web.ignoring()
-			.requestMatchers("/members/sign-up")
-			.requestMatchers("/login");
 	}
 
 	@Bean
