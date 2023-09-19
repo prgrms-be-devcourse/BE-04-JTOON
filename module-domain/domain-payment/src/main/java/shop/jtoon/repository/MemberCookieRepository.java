5dd96ec0a4d8b1f8113068fff457905c50b9f0c8
kmebin <blowhuiee@gmail.com>
2023-09-19 18:48:28 +0900

    [JT-63] refactor: memberId를 통해 쿠키 가져오는 쿼리로 수정

--- module-domain/domain-payment/src/main/java/shop/jtoon/repository/MemberCookieRepository.java
+++ module-domain/domain-payment/src/main/java/shop/jtoon/repository/MemberCookieRepository.java
@@ -3,6 +3,8 @@ package shop.jtoon.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import shop.jtoon.entity.Member;
@@ -12,4 +14,7 @@ import shop.jtoon.entity.MemberCookie;
 public interface MemberCookieRepository extends JpaRepository<MemberCookie, Long> {
 
 	Optional<MemberCookie> findByMember(Member member);
+
+	@Query("SELECT mc FROM MemberCookie mc WHERE mc.member.id = :memberId")
+	Optional<MemberCookie> findByMemberId(@Param("memberId") Long memberId);
 }
