d566c6071ffd45dc4e50eef886c724ff82f1962d
kmebin <blowhuiee@gmail.com>
2023-09-19 15:47:02 +0900

    [JT-63] fix: 웹툰 정보 조회 쿼리 수정

--- module-domain/domain-webtoon/src/main/java/shop/jtoon/repository/DayOfWeekWebtoonRepository.java
+++ module-domain/domain-webtoon/src/main/java/shop/jtoon/repository/DayOfWeekWebtoonRepository.java
@@ -1,8 +1,15 @@
 package shop.jtoon.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import shop.jtoon.entity.DayOfWeekWebtoon;
 
 public interface DayOfWeekWebtoonRepository extends JpaRepository<DayOfWeekWebtoon, Long> {
+
+	@Query("SELECT d FROM DayOfWeekWebtoon d WHERE d.webtoon.id = :webtoonId")
+	List<DayOfWeekWebtoon> findByWebtoonId(@Param("webtoonId") Long webtoonId);
 }
--- module-domain/domain-webtoon/src/main/java/shop/jtoon/repository/GenreWebtoonRepository.java
+++ module-domain/domain-webtoon/src/main/java/shop/jtoon/repository/GenreWebtoonRepository.java
@@ -1,8 +1,15 @@
 package shop.jtoon.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import shop.jtoon.entity.GenreWebtoon;
 
 public interface GenreWebtoonRepository extends JpaRepository<GenreWebtoon, Long> {
+
+	@Query("SELECT g FROM GenreWebtoon g WHERE g.webtoon.id = :webtoonId")
+	List<GenreWebtoon> findByWebtoonId(@Param("webtoonId") Long webtoonId);
 }
