ce126e5beceb3d9489b9b1b77434190e13bb2781
HyuckJuneHong <jjj8337721@naver.com>
2023-09-20 01:59:10 +0900

    [JT-76] test: cancelIamport 테스트
    - 결제 취소가 성공적으로 됐을 때, (Void)

--- module-internal/iamport-client/src/test/java/shop/jtoon/service/IamportServiceTest.java
+++ module-internal/iamport-client/src/test/java/shop/jtoon/service/IamportServiceTest.java
@@ -2,6 +2,7 @@ package shop.jtoon.service;
 
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.request.CancelData;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,8 +12,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import shop.jtoon.dto.CancelDto;
 import shop.jtoon.exception.IamportException;
 import shop.jtoon.exception.InvalidRequestException;
+import shop.jtoon.factory.CreatorFactory;
 import shop.jtoon.type.ErrorStatus;
 
 import java.io.IOException;
@@ -35,13 +38,14 @@ class IamportServiceTest {
 
     private IamportResponse<Payment> irsp;
     private Payment payment;
+    private String impUid;
 
     @BeforeEach
     public void beforeEach() {
         try {
+            impUid = "imp123";
             irsp = mock(IamportResponse.class);
             payment = mock(Payment.class);
-            given(payment.getAmount()).willReturn(BigDecimal.valueOf(1000));
             given(irsp.getResponse()).willReturn(payment);
             given(iamportClient.paymentByImpUid(any(String.class))).willReturn(irsp);
         } catch (IamportResponseException | IOException e) {
@@ -52,8 +56,11 @@ class IamportServiceTest {
     @DisplayName("validateIamport - 실제 결제된 금액과 프론트에서 보내준 금액이 다를 때, - InvalidRequestException")
     @Test
     void validateIamport_InvalidRequestException() {
+        // Given
+        given(payment.getAmount()).willReturn(BigDecimal.valueOf(1000));
+
         // When, Then
-        assertThatThrownBy(() -> iamportService.validateIamport("imp123", BigDecimal.valueOf(1)))
+        assertThatThrownBy(() -> iamportService.validateIamport(impUid, BigDecimal.valueOf(1)))
                 .isInstanceOf(InvalidRequestException.class)
                 .hasMessage(ErrorStatus.PAYMENT_AMOUNT_INVALID.getMessage());
     }
@@ -61,8 +68,24 @@ class IamportServiceTest {
     @DisplayName("validateIamport - 실제 결제된 금액과 프론트에서 보내준 금액이 같을 때, - Void")
     @Test
     void validateIamport_Void() {
+        // Given
+        given(payment.getAmount()).willReturn(BigDecimal.valueOf(1000));
+
         // When, Then
         assertThatNoException()
-                .isThrownBy(() -> iamportService.validateIamport("imp123", BigDecimal.valueOf(1000)));
+                .isThrownBy(() -> iamportService.validateIamport(impUid, BigDecimal.valueOf(1000)));
+    }
+
+    @DisplayName("cancelIamport - 결제 취소가 성공적으로 됐을 때, - Void")
+    @Test
+    void cancelIamport_Void() throws IamportResponseException, IOException {
+        // Given
+        CancelDto cancelDto = CreatorFactory.createCancelDto(impUid, 1000);
+        given(payment.getImpUid()).willReturn(impUid);
+        given(iamportClient.cancelPaymentByImpUid(any(CancelData.class))).willReturn(irsp);
+
+        // When, Then
+        assertThatNoException()
+                .isThrownBy(() -> iamportService.cancelIamport(cancelDto));
     }
 }
