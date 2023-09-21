cb1122a9c521251085d4d617b7bd0a2beddf00b9
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-09-21 23:01:37 +0900

    v1.1.0 리팩토링 배포

--- module-internal/s3-client/src/main/java/shop/jtoon/service/S3Service.java
+++ module-internal/s3-client/src/main/java/shop/jtoon/service/S3Service.java
@@ -4,20 +4,20 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import shop.jtoon.dto.UploadImageDto;
-import shop.jtoon.util.S3Client;
+import shop.jtoon.util.S3Manager;
 
 @Service
 @RequiredArgsConstructor
 public class S3Service {
 
-	private final S3Client s3Client;
+	private final S3Manager s3Manager;
 
 	public String uploadImage(UploadImageDto dto) {
 		String key = dto.toKey();
-		return s3Client.uploadImage(key, dto.image());
+		return s3Manager.uploadImage(key, dto.image());
 	}
 
 	public void deleteImage(String imageUrl) {
-		s3Client.delete(imageUrl);
+		s3Manager.delete(imageUrl);
 	}
 }
