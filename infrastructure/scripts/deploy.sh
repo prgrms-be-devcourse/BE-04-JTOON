1e500e667ae01739071efd5837fc3c1e090fa5c7
Dev Uni <wlwhsvkdlxh@gmail.com>
2023-09-21 23:37:40 +0900

    v1.1.0 최종배포

--- infrastructure/scripts/deploy.sh
+++ infrastructure/scripts/deploy.sh
@@ -28,17 +28,16 @@ if [ -n "$IS_BLUE" ]; then
             echo "4. jtoon-blue 컨테이너 종료"
             docker-compose stop jtoon-blue
 
-            echo "slack 알람 발송 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
-            json="{ \"text\": \"jtoon-green 배포 성공 !! -> 배포 성공 시각: $(date '+%Y-%m-%d %H:%M:%S')\" }"
+	          echo "5. jtoon-green 배포 성공 -> slack 성공 알람 발송"
+            json="{ \"text\": \"[성공] jtoon-green 배포 성공 !! -> 배포 성공 시각: $(TZ="Asia/Seoul" date '+%Y-%m-%d %H:%M:%S')\" }"
             curl -X POST -H 'Content-type: application/json' --data "$json" "$slack_web_hook"
 
             break;
         fi
 
         if [ $attempt -eq 24 ]; then
-            echo "jtoon-green 배포 중 문제 발생 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
-            echo "slack 알람 발송 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
-            json="{ \"text\": \"jtoon-green 배포 중 문제가 발생하여 배포가 비정상 중단되었으니 확인 부탁드립니다 -> 문제 발생 시각: $(date '+%Y-%m-%d %H:%M:%S')\" }"
+	          echo "jtoon-green 배포 실패 !! -> slack 실패 알람 발송"
+            json="{ \"text\": \"[실패] jtoon-green 배포 중 문제가 발생하여 배포가 비정상 중단되었으니 확인 부탁드립니다 -> 문제 발생 시각: $(TZ="Asia/Seoul" date '+%Y-%m-%d %H:%M:%S')\" }"
             curl -X POST -H 'Content-type: application/json' --data "$json" "$slack_web_hook"
 
             docker-compose stop jtoon-green
@@ -68,17 +67,16 @@ else
             echo "4. jtoon-green 컨테이너 종료"
             docker-compose stop jtoon-green
 
-            echo "slack 알람 발송 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
-            json="{ \"text\": \"jtoon-blue 배포 성공 !! -> 배포 성공 시각: $(date '+%Y-%m-%d %H:%M:%S')\" }"
+	          echo "5. jtoon-blue 배포 성공 -> slack 성공 알람 발송"
+            json="{ \"text\": \"[성공] jtoon-blue 배포 성공 !! -> 배포 성공 시각: $(TZ="Asia/Seoul" date '+%Y-%m-%d %H:%M:%S')\" }"
             curl -X POST -H 'Content-type: application/json' --data "$json" "$slack_web_hook"
 
             break;
         fi
 
         if [ $attempt -eq 24 ]; then
-            echo "jtoon-blue 배포 중 문제 발생 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
-            echo "slack 알람 발송 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
-            json="{ \"text\": \"jtoon-blue 배포 중 문제가 발생하여 배포가 비정상 중단되었으니 확인 부탁드립니다 -> 문제 발생 시각: $(date '+%Y-%m-%d %H:%M:%S')\" }"
+	          echo "jtoon-blue 배포 실패 !! -> slack 실패 알람 발송"
+            json="{ \"text\": \"[실패] jtoon-blue 배포 중 문제가 발생하여 배포가 비정상 중단되었으니 확인 부탁드립니다 -> 문제 발생 시각: $(TZ="Asia/Seoul" date '+%Y-%m-%d %H:%M:%S')\" }"
             curl -X POST -H 'Content-type: application/json' --data "$json" "$slack_web_hook"
 
             docker-compose stop jtoon-blue
